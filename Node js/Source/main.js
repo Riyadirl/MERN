cfe1bb51a0fefdac481866199577865a860705a9
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-05-04 03:35:49 +0600

    .

--- Node js/Source/main.js
+++ Node js/Source/main.js
@@ -11,22 +11,19 @@ console.log("server run done"); */
 
 let http = require("http");
 let server = http.createServer(function (req, res) {
-  if (req.url ==="/") {
-    req.writeHead(200, { 'Content-type': 'text/html' });
+  if (req.url === "/") {
+    req.writeHead(200, { "Content-type": "text/html" });
     res.write("<h1>This is home page</h1>");
     res.end();
-  }
-  else if (req.url === "/about") {
-    req.writeHead(200, { 'Content-type': 'text/html'  });
+  } else if (req.url === "/about") {
+    req.writeHead(200, { "Content-type": "text/html" });
     res.write("<h1>This is about page</h1>");
     res.end();
-  }
-  else if (req.url === "contact") {
-    req.writeHead(200, { 'Content-type': 'text/html'  });
+  } else if (req.url === "contact") {
+    req.writeHead(200, { "Content-type": "text/html" });
     res.write("<h1>This is content page</h1>");
     res.end();
   }
 });
-
 server.listen(5050);
 console.log("server run done");
