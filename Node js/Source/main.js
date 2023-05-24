5d557117c917818fe32f07f21623a5d4798e337c
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-05-24 20:13:10 +0600

    .

--- Node js/Source/main.js
+++ Node js/Source/main.js
@@ -9,21 +9,21 @@ let server= http.createServer(function (req, res){
 server.listen(5050);
 console.log("server run done"); */
 
-
 // 5  get request response model
 //6 Request Response Model Works in node js
+/*
 let http = require("http");
 let server = http.createServer(function (req, res) {
   if (req.url === "/") {
-    req.writeHead(200, { "Content-type": "text/html" });
+    res.writeHead(200, { "Content-type": "text/html" });
     res.write("<h1>This is home page</h1>");
     res.end();
   } else if (req.url === "/about") {
-    req.writeHead(200, { "Content-type": "text/html" });
+    res.writeHead(200, { "Content-type": "text/html" });
     res.write("<h1>This is about page</h1>");
     res.end();
   } else if (req.url === "contact") {
-    req.writeHead(200, { "Content-type": "text/html" });
+    res.writeHead(200, { "Content-type": "text/html" });
     res.write("<h1>This is content page</h1>");
     res.end();
   }
@@ -31,5 +31,58 @@ let server = http.createServer(function (req, res) {
 
 server.listen(5050);
 console.log("server run done");
+*/
+//7 Understanding Http Client
+//axios, fetch,jquery,
+//for testing >> postman
+
+//8 Understanding Package Json file
+//npm init --y
+
+//9 URL Module
+/*
+let http = require("http");
+let URL = require("url");
+
+let server = http.createServer(function (req, res) {
+  let myURL = "http://rabbil.com/blog.html?year=2015&month=june";
+
+  let myURLObj = URL.parse(myURL,true);
+
+  let myHostName= myURLObj.host;
+  let myPathName = myURLObj.pathname;
+  let mySearchName = myURLObj.search;
+
+      res.writeHead(200, {'content-type': 'text/html'})
+      res.write(myHostName);  //rabbil.com
+      res.write(myPathName);  //blog.html
+      res.write(mySearchName);  //?year=2015&month=june
+
+      res.end();
+
+});
+
+server.listen(5050);
+console.log("server run done");
+*/
+
+//10 G FS Module  file system
+//asy>> multitask
+//sy >> no multi
+
+//11 Understanding Synchronous Asynchronous
+
+//12 Understanding Synchronous Asynchronous
+
+
+
+
+
+
+
+
+
+
+
 
 
