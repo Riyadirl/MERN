6d195731bbbb1115f744bd142b3c2ad5fbc089f8
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-05-26 20:34:09 +0600

    .

--- Node js/Fs Module/index.js
+++ Node js/Fs Module/index.js
@@ -1,10 +1,12 @@
 //14 fs ready file
-
+//asynchronous read file
+/*
 let fs = require("fs");
 let http = require("http");
 
 let server = http.createServer(function (req, res) {
   if ((req.url = "/")) {
+    //asynchronous
     fs.readFile("home.html", function (err, data) {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.writeHead(data);
@@ -15,3 +17,23 @@ let server = http.createServer(function (req, res) {
 
 server.listen(4040);
 console.log("server listening successfully");
+*/
+
+//15   sync read
+let fs = require("fs");
+let http = require("http");
+
+let server = http.createServer(function (req, res) {
+  if ((req.url = "/")) {
+    let myData = fs.readFileSync("home.html", function () {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.writeHead(myData);
+      res.end();
+    });
+  }
+});
+
+server.listen(5050);
+console.log("server listening successfully");
+
+// 16   async write
