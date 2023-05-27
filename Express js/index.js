6ee6067f223c5231002fd3b3643a1986e0fa2809
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-05-27 21:02:27 +0600

    1st express app

---
+++ Express js/index.js
@@ -0,0 +1,5 @@
+//1 intro
+// server side web framework
+//developing servre side rest API
+
+//2 Express Application
---
+++ Express js/practice1/1stApp.js
@@ -0,0 +1,13 @@
+//2 Express Application
+let express = require("express");
+
+app = express();
+
+
+app.get("/", function (req, res) {
+  res.send("Hello express!");
+});
+
+app.listen(8000, function () {
+  console.log("Server Run Success");
+});
